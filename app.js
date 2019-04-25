cf47f20c936047698e9359a6a295eda8af89a02d
Paula Katerinne Campos Campos <pau.campos89@gmail.com>
2019-04-24 23:20:12 -0400

    mongoose, conexion a base de datos y rutas

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 // Requires
 var express = require('express');
+var mongoose = require('mongoose');
 
 // Inicializar variables
 var app = express();
+
+// Conexion a la base de datos
+mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
+    if (err) throw err;
+    console.log('Base de datos on port 27017: \x1b[32m%s\x1b[0m', 'online');
+});
+
+// rutas
+app.get('/', (req, res, next) => {
+    res.status(200).json({
+        ok: true,
+        mensaje: 'Petición realizada correctamente'
+    });
+});
 
 // Escuchar peticiones
 app.listen(3001, () => {
