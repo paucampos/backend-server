381312ec269b84913dbcad5079af8ffd88f66fcf
Paula Katerinne Campos Campos <pau.campos89@gmail.com>
2019-05-15 20:20:32 -0400

    autenticación google sign-in

--- config/config.js
+++ config/config.js
@@ -2,3 +2,6 @@ module.exports.SEED = 'este-es-un-seed-dificil';
 module.exports.DB_HOST = 'localhost';
 module.exports.DB_PORT = 27017;
 module.exports.DB_NAME = 'hospitalDB';
+
+// google
+module.exports.CLIENT_ID = '744966470634-r8ri8m5dd4oc5gcvbgl3n5nu92rdsl9k.apps.googleusercontent.com';
--- routes/login.js
+++ routes/login.js
@@ -1,15 +1,114 @@
-var express = require('express');
+let express = require('express');
+
 // Librería para encriptar la password
-var bcrypt = require('bcryptjs');
-var Usuario = require('./../models/usuario');
-var jwt = require('jsonwebtoken');
+let bcrypt = require('bcryptjs');
+
+let jwt = require('jsonwebtoken');
 let SEED = require('../config/config').SEED;
 
-var app = express();
+let Usuario = require('./../models/usuario');
 
+// google
+let CLIENT_ID = require('../config/config').CLIENT_ID;
+const { OAuth2Client } = require('google-auth-library');
+const client = new OAuth2Client(CLIENT_ID);
+
+let app = express();
+
+//==========================
+// Autenticación de google
+//==========================
+async function verify(token) {
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: CLIENT_ID, // Specify the CLIENT_ID of the app that accesses the backend
+        // Or, if multiple clients access the backend:
+        //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+    });
+    const payload = ticket.getPayload();
+    // const userid = payload['sub'];
+    // If request specified a G Suite domain:
+    //const domain = payload['hd'];
+    return {
+        nombre: payload.name,
+        email: payload.email,
+        img: payload.picture,
+        google: true
+    }
+}
+
+app.post('/google', async(req, res) => {
+    let token = req.body.token;
+
+    let googleUser = await verify(token)
+        .catch(error => {
+            return res.status(403).json({
+                ok: false,
+                mensaje: 'Token no válido'
+            });
+        })
+
+    Usuario.findOne({ email: googleUser.email }, (err, usuarioBD) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error al buscar usuario',
+                errors: err
+            });
+        }
+
+        if (usuarioBD) {
+            if (usuarioBD.google === false) {
+                return res.status(400).json({
+                    ok: false,
+                    mensaje: 'Debe de usar su autenticación normal'
+                });
+            } else {
+                let token = jwt.sign({ usuario: usuarioBD }, SEED, { expiresIn: 14400 }); // 4 horas
+
+                res.status(200).json({
+                    ok: true,
+                    usuario: usuarioBD,
+                    token,
+                    id: usuarioBD._id
+                });
+            }
+        } else {
+            // El usuario no existe, hay que crearlo
+            let usuario = new Usuario();
+            usuario.nombre = googleUser.nombre;
+            usuario.email = googleUser.email;
+            usuario.img = googleUser.img;
+            usuario.google = true;
+            usuario.password = ':)';
+
+            usuario.save((err, usuarioBD) => {
+                let token = jwt.sign({ usuario: usuarioBD }, SEED, { expiresIn: 14400 }); // 4 horas
+
+                res.status(200).json({
+                    ok: true,
+                    usuario: usuarioBD,
+                    token,
+                    id: usuarioBD._id
+                });
+            });
+        }
+    });
+
+    // return res.status(200).json({
+    //     ok: true,
+    //     mensaje: 'Todo ok',
+    //     googleUser
+    // });
+});
+
+
+//======================
+// Autenticación normal
+//======================
 app.post('/', (req, res) => {
 
-    var body = req.body;
+    let body = req.body;
 
     Usuario.findOne({ email: body.email }, (err, usuarioBD) => {
         if (err) {
@@ -40,7 +139,7 @@ app.post('/', (req, res) => {
 
         // Crear un JWT: json web token
         usuarioBD.password = ';)';
-        var token = jwt.sign({ usuario: usuarioBD }, SEED, { expiresIn: 14400 }); // 4 horas
+        let token = jwt.sign({ usuario: usuarioBD }, SEED, { expiresIn: 14400 }); // 4 horas
 
 
         res.status(200).json({
